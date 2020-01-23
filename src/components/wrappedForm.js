fd0feb7f48d3bc679fab0b2490f1eefd14e54c94
YelloDev <48722315+yellodev@users.noreply.github.com>
2020-01-23 12:04:26 +0400

    Vince Commit

---
+++ src/components/wrappedForm.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class wrappedForm extends React.Component {
+  render() {
+    return (
+      <>
+        <p> Met ton formulaire ici </p>
+      </>
+    )
+  }
+}
+
+export default wrappedForm
--- src/pages/merci.js
+++ src/pages/merci.js
@@ -1,43 +1,19 @@
 import React from "react"
 
-
 import Layout from "../components/layout"
 
 import SEO from "../components/seo"
 
-
-
 class merci extends React.Component {
-
   render() {
-
-
-
     return (
-
       <Layout>
         <SEO title="Merci !" />
-        
-        <h1> Présentation </h1>
 
-        <p> Sur ce site vous pouvez vous inscrire et connecter !</p>
+        <h1> Nom et Prénom </h1>
 
-        <p>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio!
-        Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt,
-        esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.</p>
-
-        <p>Paphius quin etiam et Cornelius senatores, ambo venenorum artibus pravis se polluisse confessi,
-        eodem pronuntiante Maximino sunt interfecti. pari sorte etiam procurator monetae extinctus est. Sericum enim et Asbolium supra dictos,quoniam cum hortaretur
-        passim nominare, quos vellent, adiecta religione firmarat, nullum igni vel ferro se puniri iussurum, plumbi validis ictibus 
-        interemit. et post hoe flammis Campensem aruspicem dedit, in negotio eius nullo sacramento constrictus.</p>
-
-        <p>Harum trium sententiarum nulli prorsus assentior. Nec enim illa prima vera est, ut, quem ad modum in se quisque sit, sic in amicum sit animatus.
-        Quam multa enim, quae nostra causa numquam faceremus, facimus causa amicorum! precari ab indigno, supplicare, tum acerbius in aliquem invehi
-        insectarique vehementius, quae in nostris rebus non satis honeste, in amicorum fiunt honestissime; multaeque res sunt in quibus de suis commodis
-        viri boni multa detrahunt detrahique patiuntur, ut iis amici potius quam ipsi fruantur.</p>
-        
+        <p> Merci de votre participation !</p>
       </Layout>
-
     )
   }
 }
