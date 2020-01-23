13dce05e0631792ab07fbff29f7b6cf2fa6b586d
YelloDev <48722315+yellodev@users.noreply.github.com>
2020-01-23 13:26:32 +0400

    Update formulaire

--- src/pages/merci.js
+++ src/pages/merci.js
@@ -6,13 +6,16 @@ import SEO from "../components/seo"
 
 class merci extends React.Component {
   render() {
+
+    const { nom, prenom, email } = this.props.location.state
+
     return (
       <Layout>
         <SEO title="Merci !" />
 
-        <h1> Nom et Prénom </h1>
+        <h1>Merci pour votre participation {nom} {prenom} </h1>
 
-        <p> Merci de votre participation !</p>
+        <p> Un mail vous a été envoyé à : {email} !</p>
       </Layout>
     )
   }
